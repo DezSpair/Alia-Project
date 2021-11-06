30ddd7e3e424fe5379fd66a5f5d1cff4a8867862
deza_ek <jekamails@gmail.com>
2021-11-06 18:04:03 +0300

    merge

--- src/Character/Builder/Natures.tsx
+++ src/Character/Builder/Natures.tsx
@@ -1,35 +1,36 @@
 import { observer } from "mobx-react-lite";
 import * as React from "react";
 import { useRootStore } from "../../store/rootStore";
-import Radio from '@mui/material/Radio';
-import RadioGroup from '@mui/material/RadioGroup';
-import FormControlLabel from '@mui/material/FormControlLabel';
-import FormControl from '@mui/material/FormControl';
-import FormLabel from '@mui/material/FormLabel';
-import { SubNatures } from './SubNatures';
+import Radio from "@mui/material/Radio";
+import RadioGroup from "@mui/material/RadioGroup";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import { SubNatures } from "./SubNatures";
 
 export const Natures = observer(() => {
-    let selected = '' as string;
-    const { character } = useRootStore();
-    const handleRadioChange = (event: any) => {
-        character.nature = event.target.value;
-    };
+  const { character } = useRootStore();
+  const handleRadioChange = (event: any) => {
+    character.nature = event.target.value;
+  };
+  const natures = ["Рассудительность", "Импульсивность", "Самоотверженность", "Эгоизм"];
+  return (
+    <div>
+      <RadioGroup
+        row
+        name="row-radio-buttons-group"
+        onChange={handleRadioChange}
+      >
+          {natures.map((item, index) => {
+              return (
+                <FormControlLabel
+                    value={item}
+                    control={<Radio />}
+                    label={item}
+                    key={"nature_" + index}
+                />);
+          })}
+      </RadioGroup>
 
-    return (
-        <div>
-            <RadioGroup 
-            row 
-            name="row-radio-buttons-group"
-            onChange={handleRadioChange}
-            >
-                <FormControlLabel value="Рассудительность" control={<Radio />} label="Рассудительность" />
-                <FormControlLabel value="Импульсивность" control={<Radio />} label="Импульсивность" />
-                <FormControlLabel value="Самоотверженность" control={<Radio />} label="Самоотверженность" />
-                <FormControlLabel value="Эгоизм" control={<Radio />} label="Эгоизм" />        
-            </RadioGroup>
-
-            {character.nature != '' && <SubNatures />}
-        </div>
-
-    );
+      {character.nature && <SubNatures />}
+    </div>
+  );
 });
